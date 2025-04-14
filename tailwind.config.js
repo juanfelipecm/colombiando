0402c460eda4d090e08ae2b5862de51c7ec02099
Juan Felipe Castillo <juanfelipe.22@gmail.com>
2025-04-13 21:31:50 -0500

    Iniciar tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        'colombiando-yellow': '#FFB100',
+        'colombiando-blue': '#0060BB',
+        'colombiando-red': '#D00000',
+      },
       fontFamily: {
         sans: ['Montserrat', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
       },
